31661b0b5c84f4beb5b54d3f1a1e233be2fa1212
Naingwin <naingwin.dev@email.com>
2024-09-07 10:29:12 +0630

    install

---
+++ index.ts
@@ -0,0 +1,2 @@
+const name: string = "Naing";
+name = true;
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import ListGroup from "./components/ListGroup";
+
+export default function App() {
+    return <div className="text-3xl text-red-500 font-bold underline">App</div>;
+}
